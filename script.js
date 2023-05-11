4a87bf88bfee586507066d78f788643b81edeb6d
Zachary <zachary.slupski@wp.pl>
2023-05-11 22:52:51 +0200

    Some ui changes

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
 import generateHamiltonCycle from "./hamiltonianCycleSimple.js";
 class App{
-    #rectSize = 30;
+    #rectSize = 80;
     #path;
     #appleCords={}
     #canvas;
     #ctx;
-    #board = Array(20).fill().map(() => Array(20).fill(0));;
+    #n=10;
+    #m=10;
+    #board = Array(this.#n).fill().map(() => Array(this.#m).fill(0));;
     #snake=[];
     #moveDir={x:1,y:0};
-    #n=20;
-    #m=20;
+    
 
     constructor(){
         this.#canvas = document.getElementById("canvas");
         this.#ctx = this.#canvas.getContext("2d");
-        this.#snake =[{x:30,y:0},{x:0,y:0}];
-
+        this.#snake =[{x:80,y:0},{x:0,y:0}];
+        this.#path = generateHamiltonCycle(this.#n,this.#m);
+        this._printPath();
         this._renderSnake();
         this._spawnApple();
-        this.#path = generateHamiltonCycle(this.#n,this.#m);
-
+        
         this.#board[this.#snake[0].y/this.#rectSize][this.#snake[0].x/this.#rectSize] = 2;
         this.#board[this.#snake[1].y/this.#rectSize][this.#snake[1].x/this.#rectSize] = 1;
 
-        setInterval(this._moveSnake.bind(this),10);
+        setInterval(this._moveSnake.bind(this),50);
         
     }
-    
+    _printPath()
+    {
+        this.#ctx.font = '16px sans-serif';
+        this.#ctx.fillStyle = "white";
+        this.#ctx.textAlign = "start";
+        for(let i=0;i<this.#n;i++)
+        {
+            for(let j=0;j<this.#m;j++)
+            {
+                const text = this.#path[i][j];
+                const x = j*this.#rectSize;
+                const y = i*this.#rectSize;
+                this.#ctx.fillText(text,x+this.#rectSize/2-5,y+this.#rectSize/2+5);
+            }
+        }
+    }
+    _fixNumber(x,y)
+    {
+        this.#ctx.font = '16px sans-serif';
+        this.#ctx.fillStyle = "white";
+        this.#ctx.textAlign = "start";
+        const x_idx = x/this.#rectSize;
+        const y_idx = y/this.#rectSize;
+
+        const text = this.#path[y_idx][x_idx];
+        this.#ctx.fillText(text,x+this.#rectSize/2-5,y+this.#rectSize/2+5);
+    }
     _renderSnake()
     {
         this.#snake.forEach(el => {
             const {x,y} = el;
-            this.#ctx.fillStyle = "green";
+            this.#ctx.fillStyle = "#94d82d";
             this.#ctx.beginPath();
-            this.#ctx.rect(x,y,this.#rectSize,this.#rectSize);
+            this.#ctx.rect(x+2,y+2,this.#rectSize-2,this.#rectSize-2);
             this.#ctx.fill();
             
         });
@@ -55,9 +82,13 @@ class App{
         const y = y_idx*this.#rectSize;
         
         this.#appleCords={x,y};
+
+        const center_x = x+this.#rectSize/2;
+        const center_y = y+this.#rectSize/2;
+
         this.#ctx.fillStyle = "red";
         this.#ctx.beginPath();
-        this.#ctx.rect(x,y,this.#rectSize,this.#rectSize);
+        this.#ctx.arc(center_x,center_y,this.#rectSize/4,0,Math.PI+(Math.PI*2)/2);
         this.#ctx.fill();
     }
     _eatApple(head_x,head_y)
@@ -101,6 +132,7 @@ class App{
         if(legalShortcut<0)legalShortcut=0;
         
         
+       
         let desiredShortcut = distToApple;
         if(legalShortcut>desiredShortcut)
         {
@@ -120,37 +152,26 @@ class App{
         //from all 4 directions selecting largest but less than legal shortuc
         if(canGoRight)
         {
-            let nodeDist = this._getDistance(head_val,this.#path[head_y][head_x+1]);
-           
-            
+            const nodeDist = this._getDistance(head_val,this.#path[head_y][head_x+1]);
             if(nodeDist<=legalShortcut&&nodeDist>dist)
             {
-                
                 bestDir= {x:1,y:0};
                 dist = nodeDist;
           
             }
         }
-       
         if(canGoLeft)
         {
-            
-            let nodeDist = this._getDistance(head_val,this.#path[head_y][head_x-1]);
-            
-
+            const nodeDist = this._getDistance(head_val,this.#path[head_y][head_x-1]);
             if(nodeDist<=legalShortcut&&nodeDist>dist)
-            {
-               
+            {     
                 bestDir= {x:-1,y:0};
                 dist = nodeDist;
             }
         }
-        
         if(canGoDown)
         {
-            let nodeDist = this._getDistance(head_val,this.#path[head_y+1][head_x]);
-            
-           
+            const nodeDist = this._getDistance(head_val,this.#path[head_y+1][head_x]);          
             if(nodeDist<=legalShortcut&&nodeDist>dist)
             {
                 bestDir= {x:0,y:1};
@@ -159,18 +180,22 @@ class App{
         }
         if(canGoUp)
         {
-         
-            let nodeDist = this._getDistance(head_val,this.#path[head_y-1][head_x]);
-           
+            const nodeDist = this._getDistance(head_val,this.#path[head_y-1][head_x]);           
             if(nodeDist<=legalShortcut&&nodeDist>dist)
-            {
-               
+            {               
                 bestDir= {x:0,y:-1};
                 dist = nodeDist;
             }
         }
-        
+        let temp = false;
+        console.log(bestDir,this.#moveDir);
+        if(this.#moveDir.x!= bestDir.x&&bestDir.y!=this.#moveDir.y)
+        {
+            console.log('changed')
+            temp =true;
+        }
         this.#moveDir = bestDir;
+        return temp;
     }
     _moveSnake()
     {
@@ -178,12 +203,13 @@ class App{
         calculating position of the new snake head adding it 
         to snake array and displaying it on the canvas
         */
+       
         const {x:head_x,y:head_y} = this.#snake[0];
         const head_idx_x = head_x/this.#rectSize;
         const head_idx_y = head_y/this.#rectSize;
 
         //find the best direction for snake to go
-        this._changeDir(head_idx_x,head_idx_y);
+        const changedDir = this._changeDir(head_idx_x,head_idx_y);
 
         
         const new_head_x = head_x+this.#moveDir.x*this.#rectSize;
@@ -194,13 +220,6 @@ class App{
         this.#board[new_head_idx_y][new_head_idx_x]=2;
         this.#board[head_idx_y][head_idx_x]=1;
        
-        
-        this.#snake.unshift({x:new_head_x,y:new_head_y});
-        this.#ctx.fillStyle = "green";
-        this.#ctx.beginPath();
-        this.#ctx.rect(new_head_x,new_head_y,this.#rectSize,this.#rectSize);
-        this.#ctx.fill();
-        
         if(!this._eatApple(new_head_x,new_head_y))
         {
             //making snake tail hidden
@@ -210,12 +229,31 @@ class App{
             this.#board[tail_idx_y][tail_idx_x]=0;
 
             this.#ctx.beginPath();
+            
             this.#ctx.clearRect(tail_x,tail_y,this.#rectSize,this.#rectSize);
+            this._fixNumber(tail_x,tail_y);
         }
         else{
+            this.#ctx.clearRect(this.#appleCords.x,this.#appleCords.y,this.#rectSize,this.#rectSize);
             this._spawnApple();
+            
+            
         }
-       
+        
+        this.#snake.unshift({x:new_head_x,y:new_head_y});
+        this.#ctx.fillStyle = "#94d82d";
+        this.#ctx.beginPath();
+     
+        this.#ctx.rect(new_head_x+3,new_head_y+3,this.#rectSize-3,this.#rectSize-3)
+        //else if(this.#moveDir.x!=0)this.#ctx.rect(new_head_x,new_head_y+3,this.#rectSize,this.#rectSize-3);
+        //else this.#ctx.rect(new_head_x+3,new_head_y,this.#rectSize-3,this.#rectSize);
+        //left_right
+        
+        //up down
+        this.#ctx.fill();
+        
+        
+        
     }
 }
 
